d168ef20b406f6fb5530a166b4e360a3dbac68ef
Himavarshith Reddy <himavarshithreddy@gmail.com>
2025-01-18 14:12:22 +0530

    r

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // client/src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes,Navigate } from 'react-router-dom';
 import TrackingPage from './Track';
 import Main from './Main';
 
