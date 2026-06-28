74968d89372e8397db76ce20f0aac586be2ef33f
Himavarshith Reddy <himavarshithreddy@gmail.com>
2026-06-28 22:09:30 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -3,12 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
-      { source: '/health', destination: '/api/health' },
-      { source: '/shorten', destination: '/api/shorten' },
-      { source: '/track/:path*', destination: '/api/track/:path*' },
-      { source: '/verify-password/:path*', destination: '/api/verify-password/:path*' },
-      { source: '/link-info/:path*', destination: '/api/link-info/:path*' },
-      { source: '/:code([a-zA-Z0-9_-]{5,})', destination: '/api/:code' },
+      {
+        source: '/frontend/:path*',
+        destination: '/:path*',
+      },
     ];
   },
 };
